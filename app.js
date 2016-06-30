7f2cf95b538476cfd12d4c47d822c713223e64bb
ephraimberkovitch <ephraim.berkovitch@gmail.com>
2016-06-30 06:20:24 +0300

    first working version

--- app.js
+++ app.js
@@ -4,24 +4,77 @@ angular.module('sortApp', [])
     .controller('mainController', function($scope, $http) {
         $scope.sortType     = 'name'; // set the default sort type
         $scope.sortReverse  = false;  // set the default sort order
-        $scope.searchFish   = '';     // set the default search/filter term
-
-        // create the list of sushi rolls
-        /*$scope.sushi = [
-            { name: 'Cali Roll', fish: 'Crab', tastiness: 2 },
-            { name: 'Philly', fish: 'Tuna', tastiness: 4 },
-            { name: 'Tiger', fish: 'Eel', tastiness: 7 },
-            { name: 'Rainbow', fish: 'Variety', tastiness: 6 }
-        ];*/
+        $scope.searchBook   = '';     // set the default search/filter term
+        $scope.editMode  = false;
 
         $http.get('http://127.0.0.1:5555/books')
             .then(function(res){
                 $scope.books = res.data;
+                var firstRow = {'id':-1,'writers':'','title':'','series':'','volumes':1,'copies':1,'date_created':new Date()};
+                $scope.books.unshift(firstRow);
             });
 
         $scope.sort = function(sortBy) {
             $scope.sortReverse = !$scope.sortReverse;
             $scope.sortType = sortBy;
         };
+
+        $scope.addRecord = function() {
+            $scope.editMode = true;
+            $scope.editableRowIndex = 0;
+        }
+
+        $scope.editRecord = function(book,index) {
+            $scope.editMode = true;
+            $scope.editableRowIndex = index;
+        }
+
+        $scope.confirmEdit = function(book) {
+            if (book.id==-1) {
+                //insert
+                url = 'http://127.0.0.1:5555/insert_book?w='+book.writers+'&t='+book.title+'&s='+book.series+'&v='+book.volumes+'&c='+book.copies;
+                $http.get(url)
+                    .then(function(res){
+                        $scope.books[0].id = res.data.new_id;
+                        var firstRow = {'id':-1,'writers':'','title':'','series':'','volumes':1,'copies':1,'date_created':new Date()};
+                        $scope.books.unshift(firstRow);
+                    });
+            }
+            else {
+                //update
+                url = 'http://127.0.0.1:5555/update_book?i='+book.id+'&w='+book.writers+'&t='+book.title+'&s='+book.series+'&v='+book.volumes+'&c='+book.copies;
+                console.log(url);
+                $http.get(url)
+                    .then(function(res){
+                    });
+            }
+            $scope.editMode = false;
+            $scope.editableRowIndex = -1;
+        }
+
+        $scope.cancelEdit = function(book) {
+            $scope.editMode = false;
+            $scope.editableRowIndex = -1;
+        }
+
+        $scope.deleteRecord = function(book) {
+            if (confirm('אתם בטוחים שברצונכם למחוק את הספר?')) {
+            $http.get('http://127.0.0.1:5555/delete_book?id='+book.id)
+                .then(function(res){
+                    alert('הפעולה בוצעה בהצלחה');
+                    var indexToRemove = -1;
+                    for (var i=0;i<$scope.books.length;i++)
+                    {
+                        if ($scope.books[i].id==book.id)
+                        {
+                            indexToRemove = i;
+                            break;
+                        }
+                    }
+                    if (indexToRemove>-1)
+                        $scope.books.splice(indexToRemove,1);
+                });
+            }
+        }
 
     });
--- microservices/books.py
+++ microservices/books.py
@@ -1,15 +1,52 @@
 #! /usr/bin/env python
 from flask import Flask
-from flask.ext.cors import CORS
 import books.bl.books_operations
+from datetime import datetime
 import json
+from flask import request
+from crossdomain import crossdomain
 
 app = Flask(__name__)
-CORS(app)
+
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, datetime):
+            return o.isoformat()
+
+        return json.JSONEncoder.default(self, o)
 
 @app.route("/books")
+@crossdomain(origin='*')
 def get_books():
-    return json.dumps(books.bl.books_operations.get_all_books())
+    return json.dumps(books.bl.books_operations.get_all_books(),cls=DateTimeEncoder)
+
+@app.route("/insert_book")
+@crossdomain(origin='*')
+def insert_book():
+    writers = request.args['w']
+    title = request.args['t']
+    series = request.args['s']
+    volumes = request.args['v']
+    copies = request.args['c']
+    return json.dumps(books.bl.books_operations.insert_book(writers,title,series,volumes,copies))
+
+@app.route("/delete_book")
+@crossdomain(origin='*')
+def delete_book():
+    books.bl.books_operations.delete_book(request.args['id'])
+    return "Ok"
+
+@app.route("/update_book")
+@crossdomain(origin='*')
+def update_book():
+    id = request.args['i']
+    writers = request.args['w']
+    title = request.args['t']
+    series = request.args['s']
+    volumes = request.args['v']
+    copies = request.args['c']
+    books.bl.books_operations.update_book(id,writers,title,series,volumes,copies)
+    return'Ok'
 
 if __name__ == '__main__':
     app.run(debug=True,port=5555)
